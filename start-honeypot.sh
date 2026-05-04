3dc6031c9b078858edc5b68588696ebbf6549fee
Lovish <lovishbhagat123456@gmail.com>
2026-05-04 23:41:10 +0530

    Add files via upload

---
+++ start-honeypot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Starting Healthcare IoT Honeypot..."
+
+if ! command -v docker &> /dev/null
+then
+    echo "Docker is not installed."
+    exit 1
+fi
+
+unset DOCKER_HOST
+unset DOCKER_TLS_VERIFY
+unset DOCKER_CERT_PATH
+
+if ! docker-compose up -d --build
+then
+    echo "Docker Compose failed."
+    exit 1
+fi
+
+echo ""
+echo "Honeypot Running!"
+echo "SSH    : localhost:2222"
+echo "Telnet : localhost:2223"
+
+docker-compose ps
